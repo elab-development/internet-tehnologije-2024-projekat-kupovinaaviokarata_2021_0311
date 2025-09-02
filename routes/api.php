d21003e12fed2c20cfa6e80bbfce988cc9273c7a
Marija Rajkovic <marijaaa.majaaa@gmail.com>
2025-09-02 13:58:19 +0200

    Dodat role middleware: admin i user, testirano sa tokenima

--- routes/api.php
+++ routes/api.php
@@ -18,20 +18,40 @@ Route::get('/letovi/{id}', [LetController::class, 'show']);
 Route::get('/slobodna-sedista', [LockedSeatController::class, 'slobodnaSedista']);
 
 
+
+
+
+
+
 Route::middleware('auth:sanctum')->group(function () {
     
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me',      [AuthController::class, 'me']);
 
-    
-    Route::apiResource('letovi', LetController::class)->only(['store','update','destroy']);
+    Route::middleware('role:admin')->group(function () {
+        Route::apiResource('letovi', LetController::class)->only(['store','update','destroy']);
+    });
 
-   
-    Route::get('/rezervacije', [RezervacijaController::class, 'index']);      
-    Route::get('/rezervacije/{id}', [RezervacijaController::class, 'show']);
-    Route::post('/rezervacije', [RezervacijaController::class, 'store']);
-    Route::delete('/rezervacije/{id}', [RezervacijaController::class, 'destroy']);
 
-    
-    Route::post('/zakljucaj-sediste', [LockedSeatController::class, 'lock']);
+  //  Route::apiResource('letovi', LetController::class)->only(['store','update','destroy']);
+
+
+   Route::middleware('role:user')->group(function () {
+        Route::get('/rezervacije', [RezervacijaController::class, 'index']);      
+        Route::get('/rezervacije/{id}', [RezervacijaController::class, 'show']);
+        Route::post('/rezervacije', [RezervacijaController::class, 'store']);
+        Route::delete('/rezervacije/{id}', [RezervacijaController::class, 'destroy']);
+        
+        Route::post('/zakljucaj-sediste', [LockedSeatController::class, 'lock']);
+    });
 });
+
+
+//test
+Route::get('/admin-only', function () {
+    return response()->json(['message' => 'Dobrodošao admin!']);
+})->middleware(['auth:sanctum', 'role:admin']);
+
+Route::get('/user-only', function () {
+    return response()->json(['message' => 'Dobrodošao korisniče!']);
+})->middleware(['auth:sanctum', 'role:user']);
