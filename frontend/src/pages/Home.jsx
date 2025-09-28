7176df6b2106c35f2dc88d54bd8ae1086f88f743
Milica Radenkovic <milica.radenkovic2002@gmail.com>
2025-09-28 11:43:19 +0200

    Dodata Hero sekcija

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import flightService from "../api/flightService";
 import Button from "../components/ui/Button"; 
 import Input from "../components/ui/Input";
 import Card from "../components/ui/Card";
+import Hero from "../components/ui/Hero";
 
 const Home = () => {
   const [polazna, setPolazna] = useState("");
@@ -13,15 +14,11 @@ const Home = () => {
 
   const handleSearch = async () => {
     try {
-
       const response = await flightService.getAllFlights();
-      console.log("Response from backend:",response);
-      console.log("Response data:", response.data);
-
-      const flightsArray=response.data.data;
+      const flightsArray = response.data.data;
 
       const filteredFlights = flightsArray.filter((letObj) => {
-        const letDatum = letObj.vreme_poletanja.split(" ")[0]; 
+        const letDatum = letObj.vreme_poletanja.split(" ")[0];
         return (
           letObj.polaziste.toLowerCase().includes(polazna.toLowerCase()) &&
           letObj.odrediste.toLowerCase().includes(odrediste.toLowerCase()) &&
@@ -29,7 +26,6 @@ const Home = () => {
         );
       });
 
-      
       navigate("/rezultati", { state: { flights: filteredFlights } });
     } catch (error) {
       console.error("Greška prilikom pretrage:", error);
@@ -37,32 +33,36 @@ const Home = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto mt-10">
-      <h1 className="text-3xl font-bold mb-6 text-center">Kupovina avio karata</h1>
-      <Card>
-        <Input
-          label="Polazna destinacija"
-          placeholder="Unesite polaznu destinaciju"
-          value={polazna}
-          onChange={(e) => setPolazna(e.target.value)}
-        />
-        <Input
-          label="Odredište"
-          placeholder="Unesite odredište"
-          value={odrediste}
-          onChange={(e) => setOdrediste(e.target.value)}
-        />
-        <Input
-          label="Datum polaska (opciono)"
-          type="date"
-          value={datum}
-          onChange={(e) => setDatum(e.target.value)}
-        />
-        <Button onClick={handleSearch} className="mt-4 w-full">
-          Pretraži
-        </Button>
-      </Card>
-    </div>
+    <>
+  <Hero />
+
+      <div className="max-w-2xl mx-auto mt-10">
+        <h1 className="text-3xl font-bold mb-6 text-center">Kupovina avio karata</h1>
+        <Card>
+          <Input
+            label="Polazna destinacija"
+            placeholder="Unesite polaznu destinaciju"
+            value={polazna}
+            onChange={(e) => setPolazna(e.target.value)}
+          />
+          <Input
+            label="Odredište"
+            placeholder="Unesite odredište"
+            value={odrediste}
+            onChange={(e) => setOdrediste(e.target.value)}
+          />
+          <Input
+            label="Datum polaska (opciono)"
+            type="date"
+            value={datum}
+            onChange={(e) => setDatum(e.target.value)}
+          />
+          <Button onClick={handleSearch} className="mt-4 w-full">
+            Pretraži
+          </Button>
+        </Card>
+      </div>
+    </>
   );
 };
 
